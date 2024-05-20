af5fb9c8411b722f71548597c98cb10d411dd3c9
Olga-Sh116 <olga.sh90@mail.ru>
2024-05-20 21:42:36 +0300

    add3

--- main.py
+++ main.py
@@ -9,35 +9,29 @@ SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Игра Тир")
 icon = pygame.image.load("утки.jpg")
 pygame.display.set_icon(icon)
-
 target_img = pygame.image.load("img/target.png")
 target_width = 80
 target_height = 80
 
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
-target_y = random.randint(0,SCREEN_HEIGHT - target_height)
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
-color = (random.randint(0, 255), random.randint(0, 255))
-
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))  # Исправлено
 
 running = True
 while running:
-    screen.fill(color)
+    SCREEN.fill(color)  # Исправлено
     for event in pygame.event.get():
-        if event.type == pygame.quit():
+        if event.type == pygame.QUIT:  # Исправлено
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
             if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
                 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
-    screen.blit(target_img,(target_x, target_y))
-
+    SCREEN.blit(target_img, (target_x, target_y))  # Исправлено
 
     pygame.display.update()
-
-
-
 
 pygame.quit()
 
