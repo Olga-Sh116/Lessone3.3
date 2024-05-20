46db0ec0f19ef7d828d5cfba0869534fe596d357
Olga-Sh116 <olga.sh90@mail.ru>
2024-05-20 17:45:22 +0300

    add 2

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import random
-
 import pygame
-
 pygame.init()
 
 SCREEN_WIDTH = 800
@@ -9,7 +7,7 @@ SCREEN_HEIGHT = 600
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Игра Тир")
-icon = pygame.image.load("img/p1_3494038_1572482d.jpg")
+icon = pygame.image.load("утки.jpg")
 pygame.display.set_icon(icon)
 
 target_img = pygame.image.load("img/target.png")
@@ -21,9 +19,25 @@ target_y = random.randint(0,SCREEN_HEIGHT - target_height)
 
 color = (random.randint(0, 255), random.randint(0, 255))
 
+
 running = True
 while running:
-    pass
+    screen.fill(color)
+    for event in pygame.event.get():
+        if event.type == pygame.quit():
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+    screen.blit(target_img,(target_x, target_y))
+
+
+    pygame.display.update()
+
+
+
 
 pygame.quit()
 
